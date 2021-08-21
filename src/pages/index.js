52d14213de173761ec669797890dcfe6cc10d83e
nufar21 <nufar21@gmail.com>
2021-08-21 21:36:23 +0300

    added image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import * as React from "react";
 import { Link } from "gatsby";
 import Layout from '../components/layout';
+import { StaticImage } from "gatsby-plugin-image";
 
 const Homepage = ()=>{
   return (
   <>
     <Layout pageTitle='Home Page' pageHead='This is the home page'>
-      <p>This is the home page of my first Gatsby site</p>
+      <p>This is the home page of my first Gatsby sites</p>
+      <StaticImage src="../images/image-01.jpg" alt="tree"/>
     </Layout>
   </>
   );
